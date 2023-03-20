1c5b625839836366dc808b581cec5a6f2c457256
rcwoshimao <mantoumiaoshen@gmail.com>
2023-03-20 00:22:00 -0700

    Initial Commit

---
+++ ScavengerHunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScavengerHunt
+//
+//  Created by Rebecca Chen on 2023/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
